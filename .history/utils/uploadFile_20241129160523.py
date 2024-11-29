ce05b86ea04bc1160297eed39f8c8f8d275db1ba
Sonam Gautam <gautamsonam011@gmail.com>
2024-11-29 16:06:18 +0530

    c

---
+++ .history/utils/uploadFile_20241129160523.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import pathlib
+from fastapi import UploadFile, HTTPException
+from datetime import datetime
+
+
+def uploadCertificatePdf(uploaded_file: UploadFile, folder: str) -> str:
+
+    os.makedirs(f"files/{folder}", mode=0o777, exist_ok=True)
+    
+
+    name, exten = os.path.splitext(uploaded_file.filename)
+    
+    # Validate that the file is a PDF
+    if exten.lower() != '.pdf':
+        raise HTTPException(status_code=400, detail="Invalid file type. Only PDF files are allowed.")
+    
+ 
+    time_stamp = int(datetime.timestamp(datetime.now()))
+    file_name = f"cert_{time_stamp}.pdf"
+    
+    # Define the destination path for the PDF file
+    dest = pathlib.Path(f"files/{folder}/{file_name}")
+    
+ 
+    with open(dest, "wb") as buffer:
+        shutil.copyfileobj(uploaded_file.file, buffer)  
+    
+    return str(dest)
